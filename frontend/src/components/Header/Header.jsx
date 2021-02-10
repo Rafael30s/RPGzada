4e724287b7b239448fbd300229144d8d12a6372c
Rafael30s <rafael.santos@bandtec.com.br>
2021-02-09 23:54:46 -0300

    Criação do projeto React

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Header extends Component {
+    // apagar() {
+    //     const indice = this.props.indice;
+    //     this.props.apagarNota(indice);
+    // }
+
+    render() {
+        return (
+            <section>
+                <header>
+                    <nav>
+                        <a href="#">Home</a>
+                        <a href="#">Login</a>
+                        <a href="#">Sobre</a>
+                    </nav>
+                </header>
+
+            </section>
+        );
+    }
+}
+
+export default Header;
